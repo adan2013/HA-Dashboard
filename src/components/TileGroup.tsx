382be86ac6f8d8744253df9ab1d41b6b582ad68e
Daniel Alberski <danielalberski@gmail.com>
2023-06-29 16:43:43 +0200

    dynamic grid row count

--- src/components/TileGroup.tsx
+++ src/components/TileGroup.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react'
+import { ReactNode, useRef, useEffect, useState } from 'react'
 import { useLayoutContext } from '../contexts/OutletContext'
 
 type TilesGroupProps = {
@@ -6,17 +6,41 @@ type TilesGroupProps = {
   children: ReactNode
 }
 
+const HEADER_HEIGHT = 30
+const TILE_SIZE_WITH_GAP = 175
+const MIN_ROW_COUNT = 2
+
 const TileGroup = ({ name, children }: TilesGroupProps) => {
   const layout = useLayoutContext()
+  const containerRef = useRef(null)
+  const [rowCount, setRowCount] = useState<number>(0)
+
+  useEffect(() => {
+    const onResize = () => {
+      const h = containerRef?.current?.clientHeight || 0
+      setRowCount(
+        Math.max(
+          Math.floor((h - HEADER_HEIGHT) / TILE_SIZE_WITH_GAP),
+          MIN_ROW_COUNT
+        )
+      )
+    }
+    onResize()
+    window.addEventListener('resize', onResize)
+    return () => window.removeEventListener('resize', onResize)
+  }, [])
 
   const extraStyles = layout?.isMobile
     ? {}
     : {
-        gridTemplateRows: 'repeat(3, 150px)'
+        gridTemplateRows: `repeat(${rowCount}, 150px)`
       }
 
   return (
-    <div className="mb-10 flex flex-col justify-center lg:mb-0 lg:mr-10">
+    <div
+      className="mb-10 flex flex-col justify-center lg:mb-0 lg:mr-10"
+      ref={containerRef}
+    >
       <div className="text-ms my-2">{name}</div>
       <div
         className="inline-grid grid-flow-row-dense grid-cols-2 gap-5 sm:grid-cols-3 md:grid-cols-4 lg:grid-flow-col-dense lg:grid-cols-none"
