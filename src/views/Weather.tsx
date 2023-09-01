1788d3598f900dfcbeb1ec5138ea35e939b46663
Daniel Alberski <danielalberski@gmail.com>
2023-09-01 21:36:05 +0200

    weather placeholder

--- src/views/Weather.tsx
+++ src/views/Weather.tsx
@@ -1,3 +1,12 @@
-const Weather = () => <h1>weather forecast</h1>
+import NightsStayIcon from '@mui/icons-material/NightsStay'
+
+const Weather = () => (
+  <div className="text-md my-10 text-center font-extrabold text-gray-500">
+    <div className="mb-2">
+      <NightsStayIcon className="!text-8xl" />
+    </div>
+    NO FORECAST AVAILABLE
+  </div>
+)
 
 export default Weather
