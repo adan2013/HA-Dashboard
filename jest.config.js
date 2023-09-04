1b39ed89a65a32286e5b1c00d2be2774a80c9b7f
Daniel Alberski <danielalberski@gmail.com>
2023-09-04 16:50:15 +0200

    layout tests

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,9 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   setupFiles: ['./testSetup.js'],
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect']
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  moduleNameMapper: {
+    '\\.(css|less|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
+    '\\.(gif|ttf|eot|svg)$': '<rootDir>/__mocks__/fileMock.js'
+  }
 }
--- src/utils/testUtils.tsx
+++ src/utils/testUtils.tsx
@@ -2,3 +2,11 @@ export const holdTest = (ms: number) =>
   new Promise(r => {
     setTimeout(r, ms)
   })
+
+export const defineWindowWidth = (width: number) => {
+  Object.defineProperty(window, 'innerWidth', {
+    configurable: true,
+    value: width,
+    writable: true
+  })
+}
