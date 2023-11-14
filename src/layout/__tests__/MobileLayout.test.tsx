7a2487ee4b2c4eaf8ec8a3abb6a2d1b9f5cfab56
Daniel Alberski <danielalberski@gmail.com>
2023-11-14 22:52:27 +0100

    lint fix

--- src/layout/__tests__/MobileLayout.test.tsx
+++ src/layout/__tests__/MobileLayout.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
-import { mobileMenu } from '../menus'
 import MobileLayout from '../MobileLayout'
 
 describe('MobileLayout', () => {
