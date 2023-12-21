0e222c4507056bfbb3ced00a9a5e3409a363ded3
Daniel Alberski <33758694+adan2013@users.noreply.github.com>
2023-12-21 21:55:45 +0100

    CallRemoteControlTile.tsx - migration to new backend (#16)

--- src/components/entityTiles/services/CallRemoteControlTile.tsx
+++ src/components/entityTiles/services/CallRemoteControlTile.tsx
@@ -1,29 +1,37 @@
-import { ReactElement } from 'react'
-import CallServiceTile from './CallServiceTile'
+import { ReactElement, useState } from 'react'
+import CachedOutlinedIcon from '@mui/icons-material/CachedOutlined'
+import Tile, { TileProps } from '../../basic/Tile'
+import { useBackend } from '../../../contexts/BackendContext'
 
 type CallRemoteControlTileProps = {
   title: string
   icon?: ReactElement
-  rcName: string
+  entityId: string
   button: string
 }
 
 const CallRemoteControlTile = ({
   title,
   icon,
-  rcName,
+  entityId,
   button
-}: CallRemoteControlTileProps) => (
-  <CallServiceTile
-    domain="mqtt"
-    service="publish"
-    payload={{
-      topic: `dashboard/rc/${rcName}`,
-      payload: button
-    }}
-    title={title}
-    icon={icon}
-  />
-)
+}: CallRemoteControlTileProps) => {
+  const [disabled, setDisabled] = useState(false)
+  const backend = useBackend()
+
+  const onClick = () => {
+    backend.triggerRemoteControl(entityId, button)
+    setDisabled(true)
+    setTimeout(() => setDisabled(false), 1000)
+  }
+
+  const tileData: TileProps = {
+    title,
+    icon: disabled ? <CachedOutlinedIcon /> : icon,
+    iconClassnames: disabled ? 'animate-spin opacity-30' : undefined,
+    onClick: disabled ? undefined : onClick
+  }
+  return <Tile {...tileData} />
+}
 
 export default CallRemoteControlTile
