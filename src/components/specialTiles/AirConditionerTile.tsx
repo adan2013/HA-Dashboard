d3bfb20b20970d8f0b6e076d30a2dae26a74c06c
Daniel Alberski <danielalberski@gmail.com>
2024-05-26 19:21:39 +0200

    air conditioner tile

---
+++ src/components/specialTiles/AirConditionerTile.tsx
@@ -0,0 +1,229 @@
+import clsx from 'clsx'
+import { ReactElement } from 'react'
+import RemoveIcon from '@mui/icons-material/Remove'
+import AddIcon from '@mui/icons-material/Add'
+import HdrAutoIcon from '@mui/icons-material/HdrAuto'
+import ArrowCircleDownIcon from '@mui/icons-material/ArrowCircleDown'
+import ArrowCircleUpIcon from '@mui/icons-material/ArrowCircleUp'
+import PowerSettingsNewIcon from '@mui/icons-material/PowerSettingsNew'
+import AirIcon from '@mui/icons-material/Air'
+import AcUnitIcon from '@mui/icons-material/AcUnit'
+import AutoModeIcon from '@mui/icons-material/AutoMode'
+import Tile, { TileProps } from '../basic/Tile'
+import { useHomeAssistantEntity } from '../../api/hooks'
+import { useHomeAssistant } from '../../contexts/HomeAssistantContext'
+
+type AcStatusProps = {
+  items: {
+    id: string
+    name: string
+    value: string
+  }[]
+}
+
+const AcStatus = ({ items }: AcStatusProps) => (
+  <div className="grid h-28 auto-rows-fr grid-cols-3 gap-2">
+    {items.map(({ id, name, value }) => (
+      <div
+        key={id}
+        className="flex flex-col items-center justify-center text-white"
+      >
+        <div className="text-xs">{name}</div>
+        <div className="text-2xl" data-testid={`status-${id}`}>
+          {value}
+        </div>
+      </div>
+    ))}
+  </div>
+)
+
+type AcButtonProps = {
+  icon: ReactElement
+  text?: string
+  id?: string
+  onClick?: () => void
+}
+
+const AcButton = ({ icon, text, id, onClick }: AcButtonProps) => (
+  <div
+    className={clsx(
+      'flex cursor-pointer flex-col items-center justify-center rounded-md border-2',
+      'select-none border-gray-400 bg-transparent text-white',
+      'transition-colors hover:border-white hover:bg-white hover:text-black'
+    )}
+    onClick={onClick}
+    data-testid={id ? `btn-${id}` : undefined}
+  >
+    <div>{icon}</div>
+    <div className="mt-1 text-xs">{text}</div>
+  </div>
+)
+
+type AirConditionerTileProps = {
+  title: string
+  entityId: string
+}
+
+const transformModeType = (mode: string) => {
+  switch (mode) {
+    case null:
+    case undefined:
+    case '':
+    case 'unknown':
+    case 'unavailable':
+      return '--'
+    case 'fan_only':
+      return 'Fan'
+    default:
+      return mode.charAt(0).toUpperCase() + mode.slice(1)
+  }
+}
+
+const transformFanMode = (mode: string, fanModes: string[]) => {
+  if (!!mode && !!fanModes) {
+    const fanSpeedSteps = fanModes.filter(fm => fm !== 'auto')
+    const selectedFanSpeed = fanSpeedSteps.findIndex(fm => fm === mode)
+    return selectedFanSpeed === -1
+      ? 'Auto'
+      : `${selectedFanSpeed + 1} / ${fanSpeedSteps.length}`
+  }
+  return '--'
+}
+
+const AirConditionerTile = ({ title, entityId }: AirConditionerTileProps) => {
+  const { entityState, isUnavailable } = useHomeAssistantEntity(entityId)
+  const ha = useHomeAssistant()
+
+  const mode = isUnavailable ? '--' : entityState?.state
+  const currentTemp = Math.round(
+    entityState?.attributes?.current_temperature || 0
+  )
+  const fanMode = isUnavailable ? '--' : entityState?.attributes?.fan_mode
+  const temp = Math.round(entityState?.attributes?.temperature || 0)
+
+  const transformedMode = transformModeType(mode)
+  const transformedFanMode = transformFanMode(
+    fanMode,
+    entityState?.attributes?.fan_modes
+  )
+  const transformedCurrentTemp = isUnavailable ? '--' : `${currentTemp}°`
+  const transformedTemp = isUnavailable ? '--' : `${temp}°`
+
+  const changeTemp = (step: number) => {
+    const newTemp = temp + step
+    ha.callService(entityId, 'climate', 'set_temperature', {
+      temperature: newTemp
+    })
+  }
+
+  const changeFanMode = (newFanMode: string) => {
+    ha.callService(entityId, 'climate', 'set_fan_mode', {
+      fan_mode: newFanMode
+    })
+  }
+
+  const changeFanSpeed = (step: number) => {
+    const fanSpeedSteps = entityState?.attributes?.fan_modes
+    const selectedFanSpeed = fanSpeedSteps.findIndex(fm => fm === fanMode)
+    if (selectedFanSpeed === -1) {
+      changeFanMode('auto')
+    } else {
+      let newFanSpeedIndex = selectedFanSpeed + step
+      if (newFanSpeedIndex < 0) {
+        newFanSpeedIndex = fanSpeedSteps.length - 1
+      } else if (newFanSpeedIndex >= fanSpeedSteps.length) {
+        newFanSpeedIndex = 0
+      }
+      const newFanMode = fanSpeedSteps[newFanSpeedIndex]
+      changeFanMode(newFanMode)
+    }
+  }
+
+  const changeMode = (newMode: string) => {
+    ha.callService(entityId, 'climate', 'set_hvac_mode', {
+      hvac_mode: newMode
+    })
+  }
+
+  const tileData: TileProps = {
+    title,
+    size: 'big',
+    customBody: (
+      <div className="absolute bottom-0 left-0 flex h-72 w-full flex-col justify-end p-2">
+        <AcStatus
+          items={[
+            { id: 'mode', name: 'Mode', value: transformedMode },
+            {
+              id: 'current-temp',
+              name: 'Current temp',
+              value: transformedCurrentTemp
+            },
+            { id: 'fan', name: 'Fan', value: transformedFanMode }
+          ]}
+        />
+        <div className={clsx('mb-2 grid h-32 auto-rows-fr grid-cols-3 gap-2')}>
+          <AcButton
+            icon={<RemoveIcon />}
+            id="temp-down"
+            onClick={() => changeTemp(-1)}
+          />
+          <div
+            className="flex items-center justify-center text-3xl text-white"
+            data-testid="status-target-temp"
+          >
+            {transformedTemp}
+          </div>
+          <AcButton
+            icon={<AddIcon />}
+            id="temp-up"
+            onClick={() => changeTemp(1)}
+          />
+          <AcButton
+            icon={<HdrAutoIcon />}
+            id="fan-auto"
+            onClick={() => changeFanMode('auto')}
+          />
+          <AcButton
+            icon={<ArrowCircleDownIcon />}
+            id="fan-down"
+            onClick={() => changeFanSpeed(-1)}
+          />
+          <AcButton
+            icon={<ArrowCircleUpIcon />}
+            id="fan-up"
+            onClick={() => changeFanSpeed(1)}
+          />
+        </div>
+        <div className={clsx('grid h-20 auto-rows-fr grid-cols-4 gap-2')}>
+          <AcButton
+            icon={<PowerSettingsNewIcon />}
+            text="Off"
+            id="off-mode"
+            onClick={() => changeMode('off')}
+          />
+          <AcButton
+            icon={<AirIcon />}
+            text="Fan"
+            id="fan-mode"
+            onClick={() => changeMode('fan_only')}
+          />
+          <AcButton
+            icon={<AcUnitIcon />}
+            text="Cool"
+            id="cool-mode"
+            onClick={() => changeMode('cool')}
+          />
+          <AcButton
+            icon={<AutoModeIcon />}
+            text="Auto"
+            id="auto-mode"
+            onClick={() => changeMode('auto')}
+          />
+        </div>
+      </div>
+    )
+  }
+  return <Tile {...tileData} />
+}
+
+export default AirConditionerTile
---
+++ src/components/specialTiles/__tests__/AirConditionerTile.test.tsx
@@ -0,0 +1,259 @@
+import { render, screen } from '@testing-library/react'
+import { getMockedEntityState } from '../../../utils/testUtils'
+import { EntityAttributeInterface } from '../../../api/utils'
+import AirConditionerTile from '../AirConditionerTile'
+
+const AC_ENTITY_ID = 'climate.airconditioner'
+
+const defaultAttributes: Partial<EntityAttributeInterface> = {
+  current_temperature: 28,
+  temperature: 23,
+  fan_mode: 'auto',
+  fan_modes: ['auto', 'low', 'medium', 'high']
+}
+
+const callService = jest.fn()
+jest.mock('../../../contexts/HomeAssistantContext', () => ({
+  useHomeAssistant: jest.fn(() => ({
+    callService
+  }))
+}))
+
+jest.mock('../../../api/hooks', () => {
+  const originalModule = jest.requireActual('../../../api/hooks')
+
+  return {
+    __esModule: true,
+    ...originalModule,
+    useHomeAssistantEntity: jest.fn()
+  }
+})
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires,global-require
+const { useHomeAssistantEntity } = require('../../../api/hooks')
+
+const renderAirConditionerTile = (
+  state = 'off',
+  attributes = defaultAttributes,
+  isUnavailable = false
+) => {
+  useHomeAssistantEntity.mockImplementationOnce(() =>
+    isUnavailable
+      ? {
+          entityState: null,
+          isUnavailable: true
+        }
+      : getMockedEntityState(AC_ENTITY_ID, state, attributes)
+  )
+  callService.mockClear()
+  return {
+    ...render(
+      <AirConditionerTile title="Gree AC unit" entityId={AC_ENTITY_ID} />
+    ),
+    outputs: {
+      mode: screen.getByTestId('status-mode'),
+      currentTemp: screen.getByTestId('status-current-temp'),
+      fan: screen.getByTestId('status-fan'),
+      targetTemp: screen.getByTestId('status-target-temp')
+    },
+    inputs: {
+      tempDown: screen.getByTestId('btn-temp-down'),
+      tempUp: screen.getByTestId('btn-temp-up'),
+      fanAuto: screen.getByTestId('btn-fan-auto'),
+      fanDown: screen.getByTestId('btn-fan-down'),
+      fanUp: screen.getByTestId('btn-fan-up'),
+      offMode: screen.getByTestId('btn-off-mode'),
+      fanMode: screen.getByTestId('btn-fan-mode'),
+      coolMode: screen.getByTestId('btn-cool-mode'),
+      autoMode: screen.getByTestId('btn-auto-mode')
+    }
+  }
+}
+
+describe('AirConditionerTile', () => {
+  describe('status outputs', () => {
+    it('should render the tile with the correct values', () => {
+      const { outputs } = renderAirConditionerTile()
+      expect(screen.getByText('Gree AC unit')).toBeVisible()
+      expect(outputs.mode).toHaveTextContent('Off')
+      expect(outputs.currentTemp).toHaveTextContent('28°')
+      expect(outputs.fan).toHaveTextContent('Auto')
+      expect(outputs.targetTemp).toHaveTextContent('23°')
+    })
+
+    it('should render the tile with the correct values when entity is unavailable', () => {
+      const { outputs } = renderAirConditionerTile(
+        'unavailable',
+        undefined,
+        true
+      )
+      expect(screen.getByText('Gree AC unit')).toBeVisible()
+      expect(outputs.mode).toHaveTextContent('--')
+      expect(outputs.currentTemp).toHaveTextContent('--')
+      expect(outputs.fan).toHaveTextContent('--')
+      expect(outputs.targetTemp).toHaveTextContent('--')
+    })
+
+    describe('mode output', () => {
+      it.each([
+        ['--', 'unknown'],
+        ['--', 'unavailable'],
+        ['Auto', 'auto'],
+        ['Cool', 'cool'],
+        ['Fan', 'fan_only'],
+        ['Dry', 'dry'],
+        ['Off', 'off']
+      ])(
+        'should render the tile with correct %s mode if hvac is %s',
+        (expectedLabel, hvacValue) => {
+          const { outputs } = renderAirConditionerTile(
+            hvacValue,
+            defaultAttributes
+          )
+          expect(outputs.mode).toHaveTextContent(expectedLabel)
+        }
+      )
+    })
+
+    describe('fan output', () => {
+      it.each([
+        ['Auto', 'unknown'],
+        ['Auto', 'unavailable'],
+        ['Auto', 'auto'],
+        ['1 / 3', 'low'],
+        ['2 / 3', 'medium'],
+        ['3 / 3', 'high'],
+        ['Auto', 'unexpectedValue']
+      ])(
+        'should render the tile with correct %s fan mode if fanMode is %s',
+        (expectedLabel, fanMode) => {
+          const { outputs } = renderAirConditionerTile('fan', {
+            ...defaultAttributes,
+            fan_mode: fanMode
+          })
+          expect(outputs.fan).toHaveTextContent(expectedLabel)
+        }
+      )
+    })
+  })
+
+  describe('target temperature control', () => {
+    it('should decrease the target temperature', () => {
+      const { inputs } = renderAirConditionerTile()
+      inputs.tempDown.click()
+      expect(callService).toHaveBeenCalledWith(
+        AC_ENTITY_ID,
+        'climate',
+        'set_temperature',
+        {
+          temperature: 22
+        }
+      )
+    })
+
+    it('should increase the target temperature', () => {
+      const { inputs } = renderAirConditionerTile()
+      inputs.tempUp.click()
+      expect(callService).toHaveBeenCalledWith(
+        AC_ENTITY_ID,
+        'climate',
+        'set_temperature',
+        {
+          temperature: 24
+        }
+      )
+    })
+  })
+
+  describe('fan mode control', () => {
+    it('should switch fan mode to auto by clicking fan auto button', () => {
+      const { inputs } = renderAirConditionerTile()
+      inputs.fanAuto.click()
+      expect(callService).toHaveBeenCalledWith(
+        AC_ENTITY_ID,
+        'climate',
+        'set_fan_mode',
+        {
+          fan_mode: 'auto'
+        }
+      )
+    })
+
+    describe('fan down button', () => {
+      it.each([
+        ['auto', 'high'],
+        ['high', 'medium'],
+        ['medium', 'low'],
+        ['low', 'auto'],
+        ['unexpectedValue', 'auto']
+      ])(
+        'should switch fan mode from %s to %s by clicking fan down button',
+        (oldMode, newMode) => {
+          const { inputs } = renderAirConditionerTile('fan', {
+            ...defaultAttributes,
+            fan_mode: oldMode
+          })
+          inputs.fanDown.click()
+          expect(callService).toHaveBeenCalledWith(
+            AC_ENTITY_ID,
+            'climate',
+            'set_fan_mode',
+            {
+              fan_mode: newMode
+            }
+          )
+        }
+      )
+    })
+
+    describe('fan up button', () => {
+      it.each([
+        ['auto', 'low'],
+        ['low', 'medium'],
+        ['medium', 'high'],
+        ['high', 'auto'],
+        ['unexpectedValue', 'auto']
+      ])(
+        'should switch fan mode from %s to %s by clicking fan up button',
+        (oldMode, newMode) => {
+          const { inputs } = renderAirConditionerTile('fan', {
+            ...defaultAttributes,
+            fan_mode: oldMode
+          })
+          inputs.fanUp.click()
+          expect(callService).toHaveBeenCalledWith(
+            AC_ENTITY_ID,
+            'climate',
+            'set_fan_mode',
+            {
+              fan_mode: newMode
+            }
+          )
+        }
+      )
+    })
+  })
+
+  describe('mode control', () => {
+    it('should switch AC between predefined modes', () => {
+      const { inputs } = renderAirConditionerTile()
+      const expectCall = (mode: string) =>
+        expect(callService).toHaveBeenLastCalledWith(
+          AC_ENTITY_ID,
+          'climate',
+          'set_hvac_mode',
+          {
+            hvac_mode: mode
+          }
+        )
+      inputs.offMode.click()
+      expectCall('off')
+      inputs.fanMode.click()
+      expectCall('fan_only')
+      inputs.coolMode.click()
+      expectCall('cool')
+      inputs.autoMode.click()
+      expectCall('auto')
+    })
+  })
+})
