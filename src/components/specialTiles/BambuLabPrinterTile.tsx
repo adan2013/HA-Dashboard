b99395c06c0f135ea8679ba9994b729b96e289f5
Daniel Alberski <danielalberski@gmail.com>
2024-08-29 19:49:32 +0200

    BambuLabPrinterTile.tsx

---
+++ src/components/specialTiles/BambuLabPrinterTile.tsx
@@ -0,0 +1,155 @@
+import clsx from 'clsx'
+import { ReactElement, ReactNode } from 'react'
+import SupportIcon from '@mui/icons-material/Support'
+import LayersIcon from '@mui/icons-material/Layers'
+import ScheduleIcon from '@mui/icons-material/Schedule'
+import ThermostatIcon from '@mui/icons-material/Thermostat'
+import CycloneIcon from '@mui/icons-material/Cyclone'
+import LineWeightIcon from '@mui/icons-material/LineWeight'
+import SpeedIcon from '@mui/icons-material/Speed'
+import Tile, { TileProps } from '../basic/Tile'
+import { useHomeAssistantEntity } from '../../api/hooks'
+
+const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1)
+
+const transformStageValue = (s: string) => {
+  if (!s) return 'Unknown'
+  const words = s.split('_')
+  words[0] = capitalize(words[0])
+  return words.join(' ')
+}
+
+type ParamProps = {
+  icon: ReactElement
+  children: ReactNode
+}
+
+const Param = ({ icon, children }: ParamProps) => (
+  <div className="flex flex-row gap-2">
+    {icon}
+    {children}
+  </div>
+)
+
+type BambuLabPrinterTileProps = {
+  title: string
+  mainEntityId: string
+}
+
+const BambuLabPrinterTile = ({
+  title,
+  mainEntityId
+}: BambuLabPrinterTileProps) => {
+  const stageEntity = useHomeAssistantEntity(
+    `sensor.${mainEntityId}_current_stage`
+  )
+  const currentLayerEntity = useHomeAssistantEntity(
+    `sensor.${mainEntityId}_current_layer`
+  )
+  const totalLayerCountEntity = useHomeAssistantEntity(
+    `sensor.${mainEntityId}_total_layer_count`
+  )
+  const remainingTimeEntity = useHomeAssistantEntity(
+    `sensor.${mainEntityId}_remaining_time`
+  )
+  const speedProfileEntity = useHomeAssistantEntity(
+    `sensor.${mainEntityId}_speed_profile`
+  )
+  const nozzleSizeEntity = useHomeAssistantEntity(
+    `sensor.${mainEntityId}_nozzle_size`
+  )
+  const nozzleTempEntity = useHomeAssistantEntity(
+    `sensor.${mainEntityId}_nozzle_temperature`
+  )
+  const nozzleTargetTempEntity = useHomeAssistantEntity(
+    `sensor.${mainEntityId}_nozzle_target_temperature`
+  )
+  const bedTempEntity = useHomeAssistantEntity(
+    `sensor.${mainEntityId}_bed_temperature`
+  )
+  const bedTargetTempEntity = useHomeAssistantEntity(
+    `sensor.${mainEntityId}_target_bed_temperature`
+  )
+  const auxFanEntity = useHomeAssistantEntity(
+    `sensor.${mainEntityId}_aux_fan_speed`
+  )
+  const chamberFanEntity = useHomeAssistantEntity(
+    `sensor.${mainEntityId}_chamber_fan_speed`
+  )
+  const partFanEntity = useHomeAssistantEntity(
+    `sensor.${mainEntityId}_cooling_fan_speed`
+  )
+  const activeTrayEntity = useHomeAssistantEntity(
+    `sensor.${mainEntityId}_active_tray`
+  )
+
+  const stage = transformStageValue(stageEntity.entityState?.state)
+  const currentLayer = Number(currentLayerEntity.entityState?.state) || '-'
+  const totalLayerCount =
+    Number(totalLayerCountEntity.entityState?.state) || '-'
+  const remainingTime =
+    Number(remainingTimeEntity.entityState?.state) > 0
+      ? remainingTimeEntity.entityState?.state
+      : '-- : --'
+  const speedProfile = speedProfileEntity.entityState?.state || 'Unknown'
+  const nozzleSize = nozzleSizeEntity.entityState?.state
+    ? `${nozzleSizeEntity.entityState?.state} mm`
+    : '--'
+  const nozzleTemp = Number(nozzleTempEntity.entityState?.state) || '--'
+  const nozzleTargetTemp =
+    Number(nozzleTargetTempEntity.entityState?.state) || 0
+  const bedTemp = Number(bedTempEntity.entityState?.state) || '--'
+  const bedTargetTemp = Number(bedTargetTempEntity.entityState?.state) || 0
+  const auxFanSpeed = Number(auxFanEntity.entityState?.state) || 0
+  const chamberFanSpeed = Number(chamberFanEntity.entityState?.state) || 0
+  const partFanSpeed = Number(partFanEntity.entityState?.state) || 0
+  const activeTray = activeTrayEntity.isUnavailable
+    ? 'Not selected'
+    : activeTrayEntity.entityState?.state
+
+  const tileData: TileProps = {
+    title,
+    size: 'big',
+    customBody: (
+      <div className="absolute bottom-0 left-0 h-64 w-full p-2">
+        <div className="flex flex-col gap-4">
+          <div className="font-bold">{stage}</div>
+          <div className="grid grid-cols-2 gap-3">
+            <Param
+              icon={<LayersIcon />}
+            >{`${currentLayer} / ${totalLayerCount}`}</Param>
+            <Param icon={<ScheduleIcon />}>{remainingTime}</Param>
+            <Param icon={<SpeedIcon />}>{capitalize(speedProfile)}</Param>
+            <Param icon={<LineWeightIcon />}>{nozzleSize}</Param>
+            <Param icon={<ThermostatIcon />}>
+              <div className="flex flex-col gap-1">
+                <div>
+                  <div className="text-xs">Nozzle</div>
+                  {`${nozzleTemp} → ${nozzleTargetTemp}°C`}
+                </div>
+                <div>
+                  <div className="text-xs">Bed</div>
+                  {`${bedTemp} → ${bedTargetTemp}°C`}
+                </div>
+              </div>
+            </Param>
+            <Param icon={<CycloneIcon />}>
+              <div className="grid w-full grid-cols-2 gap-1">
+                <div>Aux</div>
+                <div>{`${auxFanSpeed}%`}</div>
+                <div>Cham</div>
+                <div>{`${chamberFanSpeed}%`}</div>
+                <div>Part</div>
+                <div>{`${partFanSpeed}%`}</div>
+              </div>
+            </Param>
+          </div>
+          <Param icon={<SupportIcon />}>{activeTray}</Param>
+        </div>
+      </div>
+    )
+  }
+  return <Tile {...tileData} />
+}
+
+export default BambuLabPrinterTile
--- src/views/section/Automations.tsx
+++ src/views/section/Automations.tsx
@@ -7,6 +7,8 @@ import SwitchTile from '../../components/entityTiles/switch/SwitchTile'
 import { PowerChartTile } from '../../components/entityTiles/switch/PlugTile'
 import NumericValueTile from '../../components/entityTiles/general/NumericValueTile'
 import EnergyMonitorTile from '../../components/entityTiles/switch/EnergyMonitorTile'
+import BambuLabPrinterTile from '../../components/specialTiles/BambuLabPrinterTile'
+import PlaceholderTile from '../../PlaceholderTile'
 
 const Automations = () => (
   <TileSection>
@@ -29,7 +31,6 @@ const Automations = () => (
         deviceName="bambuLabPrinter"
         title="3D printer energy monitor"
       />
-
       <ToggleHelperTile
         title="Motion sensor"
         entityId="binary_sensor.kitchenmotionsensor_occupancy"
@@ -43,11 +44,19 @@ const Automations = () => (
         entityId="sensor.kitchenmotionsensor_illuminance_lux"
         unit="lux"
       />
+      <BambuLabPrinterTile
+        title="Bambu Lab P1S"
+        mainEntityId="p1s_01p00a453001011"
+      />
       <SwitchTile
         title="Printer plug"
         entityId="switch.bambulabprinterplug"
         confirmationRequired
       />
+      <PlaceholderTile title="Auto power off" size="standard" />
+      <PlaceholderTile title="Pause current task" size="standard" />
+      <PlaceholderTile title="Chamber light" size="standard" />
+      <PlaceholderTile title="Camera view" size="standard" />
     </TileGroup>
     <TileGroup name="Bathroom">
       <PowerChartTile
